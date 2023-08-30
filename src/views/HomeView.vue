115ccd43fc32edd260d6fdc6e171be3207c65fa3
Jason <jescobar@consortiumlegal.com>
2023-08-30 15:02:48 -0600

    added link to cuadros view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -83,10 +83,39 @@ export default {
 
 <template>
  
-    <div class="navbar bg-base-100 flex justify-center">
-        <!-- <div class="navbar-center"> -->
+    <!-- <div class="navbar bg-base-100">
+        <div class="navbar-start">
             <a class="btn btn-ghost normal-case text-4xl"> Creacion de cuadro </a>
-        <!-- </div> -->
+        </div>
+        <div class="navbar-center">
+            
+        </div>
+        <div class="navbar-end">
+        </div>
+    </div> -->
+    <!-- <a class="btn btn-ghost normal-case text-4xl"> Creacion de cuadro </a> -->
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+            <div class="dropdown">
+                <label tabindex="0" class="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                
+                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-56">
+                    <li><a class="btn btn-ghost normal-case text-xl"> Lista de cuadros </a></li>
+                </ul>
+            </div>
+            <!-- <a class="btn btn-ghost normal-case text-xl hidden lg:flex"> Lista de cuadros </a> -->
+            <router-link :to="{ name: 'cuadros'}" class="btn btn-ghost normal-case text-xl hidden lg:flex">
+                Lista de cuadros
+            </router-link>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+            <a class="btn btn-ghost normal-case text-xl"> Creacion de cuadro </a>
+        </div>
+        <div class="navbar-end">
+            <!-- <a class="btn">Button</a> -->
+        </div>
     </div>
     <div class="container w-full mx-auto">
         
